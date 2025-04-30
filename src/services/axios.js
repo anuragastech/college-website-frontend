8ecb7051a3ef54a33a921bb8d242a5885b739e0a
Anurag <anuragraveendren98@gmail.com>
2025-05-01 01:15:35 +0530

    updated done

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5888',
+  baseURL: 'https://college-website-backend-soiv.onrender.com',
 });
 
 export default instance;
