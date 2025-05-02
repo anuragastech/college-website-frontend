ce21d21d21e7d0fc2d1fda608845ed3b2061fb57
Anurag <anuragraveendren98@gmail.com>
2025-05-03 01:36:10 +0530

    updated done

--- src/pages/components/NavbarHome.jsx
+++ src/pages/components/NavbarHome.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Logo & Title */}
-          <Link to="/home" className="flex items-center gap-3">
+          <Link to="/" className="flex items-center gap-3">
             <img
               src="https://cdn-icons-png.flaticon.com/512/3135/3135706.png"
               alt="College Logo"
